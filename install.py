42ccdec49ff5ca5a9e55019b6c698621f0dd5408
miguelespada <miguel.valero.espada@gmail.com>
2020-09-22 12:15:09 +0200

    add utils

--- install.py
+++ install.py
@@ -1,3 +1,4 @@
+import os
 os.system("mkdir 'models'")
 os.system("wget https://www.dropbox.com/s/zkehq1uwahhbc2o/ColorizeArtistic_gen.pth?dl=0 -O ./models/ColorizeArtistic_gen.pth")
 os.system("gdown --id '1GwKMXS1B6kFHmr1e3ArTzZClXx_hv2tm'")
---
+++ porn_utils.py
@@ -0,0 +1,10 @@
+
+def get_porn_image_colorizer():
+    artistic = get_artistic_image_colorizer(render_factor=35)
+    porn = get_artistic_image_colorizer(weights_name = "ColorizePorn_gen", render_factor=20)
+    return  (artistic, porn)
+
+def colorize_image(colorizer, path):
+    a = colorizer[0].get_transformed_image(path)
+    p = colorizer[1].get_transformed_image(path)
+    return Image.blend(a, p, 0.5) 
